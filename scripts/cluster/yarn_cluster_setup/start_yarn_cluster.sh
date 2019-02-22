f49082d705337bfc301ff71239cb942be5c1655b
pranavmrane <pmr5279@g.rit.edu>
2019-02-22 06:40:14 -0500

    Global Variables Integrated

--- scripts/cluster/yarn_cluster_setup/start_yarn_cluster.sh
+++ scripts/cluster/yarn_cluster_setup/start_yarn_cluster.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
 echo "STARTING HADOOP SERVICES"
-/usr/local/hadoop/sbin/start-dfs.sh
 
-/usr/local/hadoop/sbin/start-yarn.sh
+$HADOOP_HOME/sbin/start-dfs.sh
 
-/usr/local/hadoop/sbin/mr-jobhistory-daemon.sh start historyserver
+$HADOOP_HOME/sbin/start-yarn.sh
 
-/usr/local/hadoop/bin/hdfs dfsadmin -safemode leave
+$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
+
+$HADOOP_HOME/bin/hdfs dfsadmin -safemode leave
 
 echo "STARTING SPARK SERVICES"
-/spark-3.0.0-SNAPSHOT-bin-SparkFHE/sbin/start-all.sh
+SPARK_HOME/sbin/start-all.sh
 
 echo "RUN jps - Java Virtual Machine Process Status Tool"
 jps
 
 echo "Get basic filesystem information and statistics."
-/usr/local/hadoop/bin/hdfs dfsadmin -report
+$HADOOP_HOME/bin/hdfs dfsadmin -report
 
 echo "Yarn Cluster is Active"
+
+master_node_ip_address=`hostname -i`
+
+echo "YARN Interface Available At: "$master_node_ip_address":8088/"
+echo "Spark Interface Available At: "$master_node_ip_address":8080/"
+echo "NameNode Interface Available At: "$master_node_ip_address":50070/"
+echo "Job Master Interface Available At: "$master_node_ip_address":19888/"
--- scripts/cluster/yarn_cluster_setup/test_scripts/run_spark_test_job_pi.sh
+++ scripts/cluster/yarn_cluster_setup/test_scripts/run_spark_test_job_pi.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 echo "SPARK TEST"
-/spark-3.0.0-SNAPSHOT-bin-SparkFHE/bin/spark-submit --class org.apache.spark.examples.SparkPi  \
+$SPARK_HOME/bin/spark-submit --class org.apache.spark.examples.SparkPi  \
     --master yarn \
     --deploy-mode cluster \
     --num-executors 1 \
     --driver-memory 1g \
     --executor-memory 512m \
     --executor-cores 1 \
-    /spark-3.0.0-SNAPSHOT-bin-SparkFHE/examples/jars/spark-examples*.jar \
+    $SPARK_HOME/examples/jars/spark-examples*.jar \
     10
+
+echo "Stop Cluster If not in Use"
