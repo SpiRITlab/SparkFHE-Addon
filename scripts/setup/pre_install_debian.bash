91e20e714decf9f5f7531c6fea65f440fc1a46d0
Peizhao Hu <peiworld@gmail.com>
2019-04-24 12:56:01 -0400

    update preinstall script

--- scripts/setup/pre_install_debian.bash
+++ scripts/setup/pre_install_debian.bash
@@ -3,7 +3,7 @@
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
-sudo apt-get install -y openjdk-8-jdk unzip libz-dev git build-essential m4 libpcre3-dev gcc-8 g++-8 cmake python-dev python-pip maven
+sudo apt-get install -y pkg-config openjdk-8-jdk unzip libz-dev git build-essential m4 libpcre3-dev gcc-8 g++-8 cmake python-dev python-pip maven
 sudo apt-get autoremove -y
 sudo apt-get clean
 
