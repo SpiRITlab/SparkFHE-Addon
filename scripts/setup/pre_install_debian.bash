f25de284077344833ff7c22a4ddbd913c99ef865
Peizhao Hu <peiworld@gmail.com>
2020-06-07 00:08:04 -0400

    update preinstall script

--- scripts/setup/pre_install_debian.bash
+++ scripts/setup/pre_install_debian.bash
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EA8CACC073C3DB2A
+sudo add-apt-repository ppa:linuxuprising/java
 sudo add-apt-repository ppa:jonathonf/gcc-9.0 -y
 sudo apt-get update
-sudo apt-get install -y pkg-config openjdk-8-jdk unzip libz-dev git build-essential m4 libpcre3-dev gcc-9 g++-9 cmake python-dev python-pip maven
+sudo apt-get install -y pkg-config openjdk-11-jdk unzip libz-dev git build-essential m4 libpcre3-dev gcc-9 g++-9 cmake python-dev python-pip maven
 sudo apt-get autoremove -y
 sudo apt-get clean
-sudo echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/" >> /etc/bash.bashrc
+sudo echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" >> /etc/bash.bashrc
 
 pip install --upgrade pip
