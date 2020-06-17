f01fa4b068d03877c6f241879269f0beb9fbe4cf
Peizhao Hu <peiworld@gmail.com>
2020-06-17 17:00:46 -0400

    remove sudo command

--- scripts/setup/pre_install_debian.bash
+++ scripts/setup/pre_install_debian.bash
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y software-properties-common
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EA8CACC073C3DB2A
-sudo add-apt-repository ppa:linuxuprising/java
-sudo add-apt-repository ppa:jonathonf/gcc-9.0 -y
-sudo apt-get update
-sudo apt-get install -y pkg-config openjdk-11-jdk unzip libz-dev git build-essential m4 libpcre3-dev gcc-9 g++-9 cmake python-dev python-pip maven
-sudo apt-get autoremove -y
-sudo apt-get clean
-sudo echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" >> ~/bash.bashrc
+apt-get install -y software-properties-common
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EA8CACC073C3DB2A
+add-apt-repository ppa:linuxuprising/java
+add-apt-repository ppa:jonathonf/gcc-9.0 -y
+apt-get update
+apt-get install -y pkg-config openjdk-11-jdk unzip libz-dev git build-essential m4 libpcre3-dev gcc-9 g++-9 cmake python-dev python-pip maven
+apt-get autoremove -y
+apt-get clean
+echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" >> ~/bash.bashrc
 
-sudo pip install --upgrade pip
+pip install --upgrade pip
