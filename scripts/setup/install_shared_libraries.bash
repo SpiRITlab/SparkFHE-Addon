126ed6f623a11ff2aef96066bec2819d9a854818
Peizhao Hu <peiworld@gmail.com>
2020-06-18 00:05:31 -0400

    fix cmake binary location

--- scripts/setup/install_shared_libraries.bash
+++ scripts/setup/install_shared_libraries.bash
@@ -325,15 +325,15 @@ export LD_LIBRARY_PATH=$DEPS_lib
 if [ ! -f "/proc/1/cgroup" ] || [ "$(grep 'docker\|lxc' /proc/1/cgroup)" == "" ] ; then
   # if not, install dependencies into a self-contained folder
   DEPS_ROOT=$DEPS_ROOT
+  CMAKE_EXE=$DEPS_bin/cmake
 else
   # if docker container, install dependencies into /usr/local
   DEPS_ROOT=/usr/local
+  CMAKE_EXE=cmake
 fi
-
 
 installMinRequiredCmake() {
   CMAKE="CMAKE"
-  CMAKE_EXE=$DEPS_bin/cmake
   if [ -d $CMAKE ]; then
     if [ ! -f $CMAKE/$Marker ]; then
       rm -rf $CMAKE # remove the folder
